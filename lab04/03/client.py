01efadb922f62dca925bc2c35bbb82cff097ee8a
Phelipe Alves <sougyn@gmail.com>
2017-04-26 13:30:53 -0300

    [EX03] - Adicionando Exercício 3

--- lab04/03/client.py
+++ lab04/03/client.py
@@ -14,7 +14,6 @@ sock = socket.socket(ipv4, udp)
 # Define the port on which you want to connect
 port = 12345
 
-sock.bind(('localhost', 44444))
 # Send the data
 sock.sendto("Hi Server", ('127.0.0.1', port))
 
