3ae9e325351b8ff626251fd4118665a446a4704b
Phelipe Alves <sougyn@gmail.com>
2017-04-26 13:52:15 -0300

    [EX03] - Adicionando Exercício 3

--- lab04/03/server.py
+++ lab04/03/server.py
@@ -3,26 +3,28 @@
 
 # Socket Python Doc: https://docs.python.org/2.7/library/socket.html
 
-# first of all import the socket library
+# First of all import the socket library
 import socket
 
-# next create a socket object
+# Next create a socket object
 ipv4 = socket.AF_INET
 udp = socket.SOCK_DGRAM
 sock = socket.socket(ipv4, udp)
 print "Socket successfully created"
 
-# reserve a port on your computer in our
+# Reserve a port on your computer in our
 # case it is 12345 but it can be anything
 port = 12345
 
-# Next bind to the port
+# Next bind to the port to server
 sock.bind(('127.0.0.1', port))
 print "socket binded to %s" %(port)
 
-data, addr = sock.recvfrom(1024)
-print 'Got connection from', addr
+while True:
+    # Connect and receive data from client
+    data, addr = sock.recvfrom(1024)
+    print 'Got connection from', addr
 
-# Send the data
-print data
-sock.sendto(str('Hi Client'), addr)
+    # Send the data to client
+    print data
+    sock.sendto(str('Hi Client'), addr)
