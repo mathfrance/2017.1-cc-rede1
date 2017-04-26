04610d3b4cf79fee9063fb49ccc48467fb714af4
Phelipe Alves <sougyn@gmail.com>
2017-04-26 13:33:08 -0300

    [EX01] - Adicionando Exercício 1

--- lab04/01/server.py
+++ lab04/01/server.py
@@ -15,26 +15,19 @@ print "Socket successfully created"
 port = 12345
 
 # Next bind to the port
-# we have not typed any ip in the ip field
-# instead we have inputted an empty string
-# this makes the server listen to requests
-# coming from other computers on the network
-sock.bind(('', port))
+sock.bind(('localhost', port))
 print "socket binded to %s" %(port)
 
 # put the socket into listening mode
 sock.listen(1)
 print "socket is listening"
 
-# a forever loop until we interrupt it or
-# an error occurs
-while True:
-    # Establish connection with client.
-    conn, addr = sock.accept()
-    print 'Got connection from', addr
+# Establish connection with client.
+conn, addr = sock.accept()
+print 'Got connection from', addr
 
-    # send a thank you message to the client.
-    conn.send('Thank you for connecting')
+# send a thank you message to the client.
+conn.send('Thank you for connecting')
 
-    # Close the connection with the client
-    conn.close()
+# Close the connection with the client
+conn.close()
